782632caba0beab4f34104e564fe73f9d4f47729
willyserge <willyserge01@gmail.com>
2019-10-09 16:07:23 +0200

    Updates

---
+++ precache-manifest.a693556b192e59c06a534a782eda6886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a939466801874b9f6774eb4513b73d6",
+    "url": "/image-search-app-React/index.html"
+  },
+  {
+    "revision": "8506f51c50643627be24",
+    "url": "/image-search-app-React/static/css/main.98545ba9.chunk.css"
+  },
+  {
+    "revision": "6921ef5035c70d88316a",
+    "url": "/image-search-app-React/static/js/2.72297bed.chunk.js"
+  },
+  {
+    "revision": "8506f51c50643627be24",
+    "url": "/image-search-app-React/static/js/main.4c3862a3.chunk.js"
+  },
+  {
+    "revision": "263b726910a3321158f9",
+    "url": "/image-search-app-React/static/js/runtime-main.a81b4921.js"
+  }
+]);
